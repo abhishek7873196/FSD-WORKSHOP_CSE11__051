37a7f8bb42d722d0df8fccf361be2dd98800ba51
Abhishek sharma <mail.abhishek0305@gmail.com>
2026-09-18 18:57:57 +0530

    apitestor

--- FrontEnd/SMS/src/main.jsx
+++ FrontEnd/SMS/src/main.jsx
@@ -1,12 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
-import Home from './components/Home.jsx'
+import ApiTester from './components/apitestor.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-    <Home/>
+    <ApiTester />
   </StrictMode>,
 )
